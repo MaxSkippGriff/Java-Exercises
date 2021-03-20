97f64be5088dcba822b130663e2af250cb152148
MaxSkippGriff <mskippergriffiths@gmail.com>
2021-03-20 14:07:27 +0000

    Update 7.BinarySearch.java

--- src/Easy-Questions/7.BinarySearch.java
+++ src/Easy-Questions/7.BinarySearch.java
@@ -3,24 +3,29 @@ import java.util.*;
 /*
     Binary Search Question
     -----------------------
+    Note: These solutions assume the array is
+    sorted
 */
 
 
 /*
-    Only Solution - Basic Binary Search
+    First Solution - Recursive Binary Search
 
     - O(log(n)) time | O(log(n)) space
 
     - O(log(n)) time because array is halved
-      until target value is found.
+      after every traversal until target value
+      is found.
 
-    - O(log(n)) space because array size
-      is halved until target value is found.
+    - O(log(n)) space because the algorithm is
+      implemented recurisively which requires
+      memory usage on the call stack.
  */
 
 class Program {
 
     public static int[] binarySearch(int[] array) {
+        // Set left and rightmost indices
         int left = 0;
         int right = array.length - 1;
         return binarySearch(array, target, left, right);
@@ -30,12 +35,16 @@ class Program {
         if (left > right) {
             return -1;
         }
+        // Set the mid-point and assign the potentialMatch to
+        // mid-point
         int middle = (left + right) / 2;
         int potentialMatch = array[middle];
 
         if (target == potentialMatch) {
             return middle;
         }
+        // If target is less than potentialMatch, move to
+        // left half of array, otherwise move to right half
         else if (target < potentialMatch) {
             return binarySearch(array, target, left, middle - 1);
         }
@@ -44,3 +53,49 @@ class Program {
         }
     }
 }
+
+/*
+    Second Solution - Iterative binary search with
+                      improved space complexity
+
+    - O(log(n)) time | O(1) space
+
+    - O(log(n)) time because array is halved
+      until target value is found.
+
+    - O(1) space because it is implemented
+      iteratively so we don't store anything
+      additional in memory.
+
+*/
+
+
+
+ class Program {
+
+      public static int binarySearch(int[] array, int target) {
+ 		   // Set leftmost and rightmost indices
+      int left = 0;
+ 		   int right = array.length - 1;
+
+ 		   // Set potentialMatch to middle of array
+ 		   while (left <= right) {
+ 			  int middle = (left + right) / 2;
+ 			  int potentialMatch = array[middle];
+ 			  // If potentialMatch equals target, return value
+ 			  if (target == potentialMatch) {
+ 				     return middle;
+ 			  }
+ 			  // If target is less than potentialMatch, move to
+ 			  // left half of array
+ 			  else if (target < potentialMatch) {
+ 				    right = middle - 1;
+      }
+ 			  // Move left pointer to right half of array
+ 			  else {
+ 				    left = middle + 1;
+ 			  }
+    }
+ 		return -1;
+ 	}
+ }
